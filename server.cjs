1f57204a3ef591a0e0b1569c24013a2091caa455
abdellah2025 <bdallhaytbnadry@gmail.com>
2026-09-22 01:17:16 +0100

    Upgrade dashboard

--- server.cjs
+++ server.cjs
@@ -26,9 +26,7 @@ const ALLOWED_ORIGINS = [
   "http://localhost:5173",
   "http://localhost:5174",
   "https://be-ensamaine.web.app",
-  'https://macours.com',
-  'https://www.macours.com',
-  'https://files.macours.com'
+  "https://2beensamaine.com",
 ];
 
 app.use(
